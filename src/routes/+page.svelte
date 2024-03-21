744c4d9a0466489eb46d4c2b5e9af9633b8e8ec2
murmuur22 <69942927+murmuurr@users.noreply.github.com>
2024-03-21 12:53:56 -0400

    PARTY TIME

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,75 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<script>
+    import { onMount } from 'svelte';
+    import { blur, fade } from 'svelte/transition';
 
-<style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.100);
+
+    $: stage = 1;
+
+    let stage1_ref;
+    let stage2_ref;
+    let time;
+    let duration;
+    const playing = (curr_time) => {
+        if (curr_time >= duration/2){ // THIS WILL EXECUTE IF THE MOUSE COMES BACK IN, PAUSING THE VIDEO AT THE WRONG TIME
+            
+            // reached apex
+            stage = 2;
+        }
     }
-</style>
+    $: playing(time);
+
+
+    let isButtonVisible = true;
+    function handleStart() {
+        stage1_ref.play();
+        isButtonVisible = false;
+    }
+
+    function handlePoop() {
+        stage = 1;
+        isButtonVisible = true;
+        console.log(stage)
+    }
+
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<div
+    style="background-image: url('/assets/end_frame.jpeg')"
+    class="
+        bg-center bg-cover blur
+        h-screen w-screen
+    "
+/>
+<div
+    class = "
+        absolute top-0 left-0
+        flex justify-center items-center
+        h-screen w-screen
+    "
+>
+    {#if stage == 1}
+        <video src="/assets/Intro.mp4" playsinline muted bind:this={stage1_ref} bind:currentTime={time} bind:duration
+            class="
+                drop-shadow-lg
+                rounded-lg
+                w-10/12
+            "
+        />
+        {#if isButtonVisible == true}
+            <button out:blur on:click={handleStart} class="absolute rounded-full bg-stone-950 text-stone-50 px-6 py-2 hover:scale-125 transition ease-in-out">Party Time</button>
+        {/if}
+    {/if}
+
+    {#if stage == 2}
+        <video in:fade src="/assets/LASERS.mp4" autoplay loop playsinline muted bind:this={stage2_ref}
+            class="
+                drop-shadow-lg
+                rounded-lg
+                w-10/12
+            "
+        />
+        <button in:blur on:click={handlePoop} class="absolute rounded-full bg-stone-950 text-stone-50 px-10 py-2 hover:scale-125 transition ease-in-out">🫵💩</button>
+    {/if}
+</div>
